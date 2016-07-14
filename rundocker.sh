61bf709136503a2a7ba180296f838d846704c40f
petecarapetyan <pete@couldbe.net>
2016-07-14 14:39:43 -0500

    fixed Dockerfile

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,4 @@
-#mvn clean package
-echo "This does not currently work, still debugging. Come back later"
-#docker build -t jammazwan/springbarebones .
-#docker run -it -d -p 54321:65432 --name jamzspringbare jammazwan/springbarebones
+mvn clean package
+docker build -t jammazwan/springbarebones .
+docker run -it -d -p 54321:65432 --name jamzspringbare jammazwan/springbarebones
+#docker logs -f $(docker ps -lq)
